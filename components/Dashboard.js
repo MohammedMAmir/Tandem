5e9d257994d285bfb14b1ae96ad8f0489e949c57
Mohammed Amir <muji-amir@hotmail.com>
2025-02-18 23:56:27 -0500

    logos

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,6 @@
 import { Fugaz_One } from 'next/font/google'
 import React from 'react'
+import Calendar from './Calendar';
 
 const fugaz = Fugaz_One({
   subsets: ["latin"], weight: ['400'],
@@ -22,6 +23,7 @@ export default function Dashboard() {
 
   return (
     <div className=' flex flex-col flex-1 gap-10 sm:gap-14 md:gap-20 '>
+      {/* Render date */}
       <div className='p-4 gap-4 grid grid-cols-3 bg-[var(--light-secondary)]
       text-[var(--light-prime)] rounded-lg'>
         {Object.keys(statuses).map((status, statusIndex) => {
@@ -33,19 +35,25 @@ export default function Dashboard() {
           )
         })}
       </div>
+
       <h4 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}>
         How do you <span className="textGradientPrime">feel</span> today?
       </h4>
+
+      {/* Render Mood Buttons*/}
       <div className='grid grid-cols-2 sm:grid-cols-5 gap-4 '>
         {Object.keys(moods).map((mood, moodIndex) => {
           return(<button className={`p-4 rounded-lg purpleShadow duration-200 bg-[var(--light-secondary)]
-             hover:bg-[var(--light-accent)] `+ (moodIndex === (Object.keys(moods).length - 1) ?
+             hover:bg-[var(--light-accent)] text-center flex flex-col items-center gap-2
+             `+ (moodIndex === (Object.keys(moods).length - 1) ?
            'col-span-2 sm:col-span-1' : ' ' )} key={moodIndex}>
-            <p className={'text-5xl sm:text-6xl md:7xl ' + fugaz.className}>{moods[mood]}</p>
+            <p className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>{moods[mood]}</p>
             <p className={'text-[var(--light-prime)] ' + fugaz.className}>{mood}</p>
           </button>)
         })}
       </div>
+
+      <Calendar />
     </div>
   )
 }
